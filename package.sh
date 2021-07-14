652a2315bd403c64b41c9462fbe52270de983997
antihax <antihax@gmail.com>
2021-07-15 01:12:53 +0200

    add docker

---
+++ package.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [[ -z "$1" ]] 
+then
+    for dir in cmd/*/; do
+        dir=${dir%/}
+        if grep -q '^package main$' $dir/*.go 2>/dev/null; then
+            echo "building $dir"
+            CGO_ENABLED=0 GOOS=linux go build -a --installsuffix cgo -o bin/$(basename $dir) ./$dir
+            docker build -t antihax/gambit-$(basename $dir) -f docker/Dockerfile.$(basename $dir) .
+        else
+            echo "(skipped $dir)"
+        fi
+    done
+else
+    dir=cmd/$1/
+        echo "building $dir"
+        CGO_ENABLED=0 GOOS=linux go build -a --installsuffix cgo -o bin/$(basename $dir) ./$dir
+        docker build -t antihax/gambit-$(basename $dir) -f docker/Dockerfile.$(basename $dir) .
+        docker push antihax/gambit-$(basename $dir)
+fi
