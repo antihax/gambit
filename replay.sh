ef83a15c8d0d745a037c8c2fcc58d25b68510cf2
antihax <antihax@gmail.com>
2021-09-06 22:13:18 +0200

    add UDP

--- replay.sh
+++ replay.sh
@@ -3,6 +3,9 @@ if [[ -z "$1" ]]
 then
     echo "missing hash"
 else
+    args=()
+    if [[ "$2" = "u" ]]; then args+=( '-u' ); fi
+
     ip=`dig @resolver4.opendns.com myip.opendns.com +short`
-    wget -O - -q https://iam.antihax.net/file/antihax-pass/raw/$1 | nc $ip 55
+    wget -O - -q https://iam.antihax.net/file/antihax-pass/raw/$1 | nc $ip 55 "${args[@]}"
 fi
